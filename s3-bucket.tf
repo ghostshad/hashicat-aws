9c0921d95dd094f3ee47d61345d57a7ee0e06d81
Sergei Morozov <sergei.morozov@astrazeneca.com>
2022-09-12 11:30:57 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
-module "s3-bucket_example_complete" {
-  source  = "app.terraform.io/astrazeneca-scp/s3-bucket/aws//examples/complete"
+module "s3-bucket" {
+  source  = "app.terraform.io/astrazeneca-scp/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = "test-az"
 }
